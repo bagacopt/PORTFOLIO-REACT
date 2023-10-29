fc3fc38af98c7b89d407764fce23c66f6571a611
Bagacinho-chan <95945441+bagacinho-chan@users.noreply.github.com>
2023-10-29 06:08:35 +0000

    a

--- src/components/js/pages/Footer.js
+++ src/components/js/pages/Footer.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
 import '../../css/Footer.css';
 
 function Footer() {
   return (
     <>
     <footer className='footer'>
-      Copyright © 2023 bagaco. All rights reserved.
+      <div className='container-footer'>
+        <div className='container-footer-text'>
+          Copyright © 2023 bagaco. All rights reserved.
+        </div>
+        
+        <div className='container-logo' href='https://github.com/bagacopt'>
+          <a href="https://github.com/bagacopt"><i class="fa-brands fa-github fa-xl"></i></a>
+        </div>
+      </div>
     </footer>
     </>
   )
